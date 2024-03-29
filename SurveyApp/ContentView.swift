64962261087b19e3b452efa8fecdb6bda102dd35
Labinot Pajaziti <38728474+Labinot98@users.noreply.github.com>
2024-03-29 13:18:02 +0100

    Initial Commit

---
+++ SurveyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SurveyApp
+//
+//  Created by Pajaziti Labinot on 29.3.24..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SurveyApp/SurveyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurveyAppApp.swift
+//  SurveyApp
+//
+//  Created by Pajaziti Labinot on 29.3.24..
+//
+
+import SwiftUI
+
+@main
+struct SurveyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
